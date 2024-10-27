802a38eb15f601612b686c6c6bf9ee689a2a12a0
Nik Hixson <nikilobe@gmail.com>
2024-10-26 21:57:10 -0700

    refactored a bit and added some comments

--- iss.js
+++ iss.js
@@ -3,16 +3,21 @@ const needle = require("needle");
 const fetchMyIP = (callback) => {
   needle.get('https://api.ipify.org?format=json', (error, response) => {
     if (error) {
+      // request error
       callback(error, null);
-    } else {
-      if (response.statusCode === 200) {
-        callback(null, response.body.ip);
-      } else {
-        callback(Error(`Status Code ${response.statusCode} when fetching IP`), null);
-      }
+      return;
     }
+    // check for non 200 status code outside the error check
+    if (response.statusCode !== 200) {
+      //storing status code error into a variable msg
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${response.body}`;
+      // pass an error with the status code
+      callback(Error(msg), null);
+      return;
+    }
+    // If no errors, pass back the IP
+    callback(null, response.body.ip);
   });
 };
-
 
 module.exports = { fetchMyIP };
