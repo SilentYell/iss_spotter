3771833f99773aa016ea74f872ff131bee26486b
Nik Hixson <nikilobe@gmail.com>
2024-10-27 14:32:16 -0700

    modified nextISSTimesForMyLocation function to check to see if times is an array and if it has any elements before using forEach to avoid potential errors.

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ nextISSTimesForMyLocation((error, times) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
+  
+  // check if times is an array and has elements
+  if (Array.isArray(times) && times.length > 0) {
+    times.forEach(pass => {
+      const date = new Date(pass.risetime * 1000);
+      console.log(`Next pass at ${date} for ${pass.duration} seconds!`);
+    });
 
-  times.forEach(pass => {
-    const riseTime = new Date(pass.risetime * 1000).toString();
-    console.log(`Next pass at ${riseTime} for ${pass.duration} seconds!`);
-  });
+  } else {
+    console.log("No passes found.");
+  }
 });
