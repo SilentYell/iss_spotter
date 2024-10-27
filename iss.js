13079265ffdb56775c393db7224e0717ba9d6010
Nik Hixson <nikilobe@gmail.com>
2024-10-27 07:49:43 -0700

    adding comments

--- iss.js
+++ iss.js
@@ -1,5 +1,6 @@
 const needle = require("needle");
 
+//function to get ip address
 const fetchMyIP = (callback) => {
   needle.get('https://api.ipify.org?format=json', (error, response) => {
     if (error) {
@@ -20,25 +21,30 @@ const fetchMyIP = (callback) => {
   });
 };
 
+//function to get coordinates for the ip address
 const fetchCoordsByIP = (ip, callback) => {
   needle.get(`https://ipwho.is/${ip}`, (error, response) => {
 
+    // Check if there was an error with the request
     if (error) {
       callback(error, null);
       return;
     }
 
     const data = response.body;
+    //check if response was successful
     if (!data.success) {
       const msg = `Success status was ${data.success}. Server message says: ${data.message} when fetching for IP ${data.ip}`;
       callback(Error(msg), null);
       return;
     }
 
+    //get latitude and longitude
     const latitude = data.latitude;
     const longitude = data.longitude;
     callback(null, {latitude, longitude});
   });
 };
 
+//exporting both functions to be used by index
 module.exports = { fetchMyIP, fetchCoordsByIP };
